3cd3c6b2c110da2c1c5223af32f9918431c52de7
MotriTN <gamerMT95@gmail.com>
2022-08-16 05:50:31 +0100

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,5 +1,9 @@
 import string
 s1=list(string.ascii_lowercase)
-s2 list(string.ascii_uppercase)
-s3 list(string.digits)
+s2=list(string.ascii_uppercase)
+s3=list(string.digits)
 s4=list(string.punctuation)
+characters_number=input("how many characters for the password ?:")
+while True:
+    try:
+        characters_number=int(characters_number)
