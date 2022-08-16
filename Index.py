1bf82ba8d7f2f6f40db8870dea19109ea4fb0a21
MotriTN <gamerMT95@gmail.com>
2022-08-16 06:05:13 +0100

    Update Index.py

--- Index.py
+++ Index.py
@@ -9,7 +9,9 @@ characters_number=input("how many characters for the password ?:")
 while True:
     try:
         characters_number=int(characters_number)
-        if charachters_number = input ("please enter the number again: ")
+        if characters_number<6:
+            print("you need at least6characters")
+            characters_number=input("please enter the number again:")
         else :
             break
     except:
@@ -19,3 +21,4 @@ random.shuffle(s1)
 random.shuffle(s2)
 random.shuffle(s3)
 random.shuffle(s4)
+print(s1)
