70e0d2e48d1d4c91d0f853a2f9fe45f2cdebd4bc
MotriTN <gamerMT95@gmail.com>
2022-08-16 05:59:24 +0100

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,4 +1,6 @@
 import string
+import random
+
 s1=list(string.ascii_lowercase)
 s2=list(string.ascii_uppercase)
 s3=list(string.digits)
@@ -10,3 +12,10 @@ while True:
         if charachters_number = input ("please enter the number again: ")
         else :
             break
+    except:
+        print("please enter numbers only")
+        character_number = input("how many characters for the password?: ")
+random.shuffle(s1)
+random.shuffle(s2)
+random.shuffle(s3)
+random.shuffle(s4)
